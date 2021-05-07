0bc82b502c1d2331fbc5b71ec8a3550fbdabf854
Oliver Meredith <mail@olivermeredith.com>
2021-05-07 18:51:28 +0100

    slug

--- gatsby-node.js
+++ gatsby-node.js
@@ -18,35 +18,35 @@ exports.createSchemaCustomization = ({ actions, schema }) => {
 }
 
 
-exports.createPages = ({ graphql, actions }) => {
-    const { createPage } = actions
+// exports.createPages = ({ graphql, actions }) => {
+//     const { createPage } = actions
 
-    // **Note:** The graphql function call returns a Promise
-    // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise for more info
-    return graphql(`
-      {
-        allMarkdownRemark{
-          edges {
-            node {
-              fields {
-                slug
-              }
-            }
-          }
-        }
-      }
-      `
-    ).then(result => {
-        result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-            createPage({
-                path: node.fields.slug,
-                component: path.resolve(`./src/templates/posts.js`),
-                context: {
-                    // Data passed to context is available
-                    // in page queries as GraphQL variables.
-                    slug: node.fields.slug,
-                },
-            })
-        })
-    })
-}
+//     // **Note:** The graphql function call returns a Promise
+//     // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise for more info
+//     return graphql(`
+//       {
+//         allMarkdownRemark{
+//           edges {
+//             node {
+//               fields {
+//                 slug
+//               }
+//             }
+//           }
+//         }
+//       }
+//       `
+//     ).then(result => {
+//         result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+//             createPage({
+//                 path: node.fields.slug,
+//                 component: path.resolve(`./src/templates/posts.js`),
+//                 context: {
+//                     // Data passed to context is available
+//                     // in page queries as GraphQL variables.
+//                     slug: node.fields.slug,
+//                 },
+//             })
+//         })
+//     })
+// }
