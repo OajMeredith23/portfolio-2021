c29891c10a65a64f5978ffb77b979ee93d4ee837
Oliver Meredith <mail@olivermeredith.com>
2021-09-23 13:59:07 +0100

    Update description

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Landing from '../components/Landing'
 const IndexPage = () => {
   return (
     <>
-      <Landing title="Oliver Meredith" description="Front-end Developer </br> User-experience Designer </br> <a href='https://www.dandad.org/awards/new-blood/2021/bbc/3878/bbc-evoke/' target='_blank' rel='noopener noreferrer'>2021 D&AD New Blood award winner</a>">
+      <Landing title="Oliver Meredith" description="Front-end Developer at The Natural History Museum, London </br> <a href='https://www.dandad.org/awards/new-blood/2021/bbc/3878/bbc-evoke/' target='_blank' rel='noopener noreferrer'>2021 D&AD New Blood award winner</a>">
       </Landing>
       <WorkList />
     </>
